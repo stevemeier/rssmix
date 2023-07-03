b1ce05bdb3527638beeca9a0f74edea3a03bd615
stevemeier <email@steve-meier.de>
2023-07-03 22:54:21 +0200

    Function for config loading and setting defaults

--- lib/functions.go
+++ lib/functions.go
@@ -5,6 +5,10 @@ package lib
 import "os"
 import "strings"
 
+import "github.com/knadh/koanf"
+import "github.com/knadh/koanf/parsers/yaml"
+import "github.com/knadh/koanf/providers/file"
+
 func Value_or_default (value interface{}, def interface{}) (interface{}) {
 	switch value.(type) {
 	case string:
@@ -49,4 +53,44 @@ func FirstN (s string, n int) string {
 		return s[:n]
 	}
 	return s
+}
+
+func LoadConfig (component string) (*koanf.Koanf, string) {
+	var k = koanf.New(".")
+
+	// Defaults (for all components)
+	k.Set("database.type", "sqlite3")
+	k.Set("database.url", "rssmix.sql")
+
+	// Component specific defaults
+	switch component {
+	case "api":
+		k.Set("listen.family", "tcp4")
+		k.Set("listen.address", "127.0.0.1:8888")
+		k.Set("id.length", 10)
+		k.Set("public.protocol", "https")
+		k.Set("public.hostname", "localhost")
+		k.Set("public.subdirs", 0)
+	case "compiler":
+		// none
+	case "fetcher":
+		k.Set("interval", 10)
+		k.Set("workdir", os.Getenv("HOME"))
+		k.Set("subdirs", 0)
+	case "publisher":
+		// none
+	}
+
+	// Try config files
+	for _, configfile := range []string{ "./"+component+".yaml",
+                                             os.Getenv("HOME")+"/etc/rssmix/"+component+".yaml",
+					     "/etc/rssmix/"+component+".yaml" } {
+
+		loaderr := k.Load(file.Provider(configfile), yaml.Parser())
+		if loaderr == nil {
+			return k, configfile
+		}
+  	}
+
+	return k, "<none>"
 }
